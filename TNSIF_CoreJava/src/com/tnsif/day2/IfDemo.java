3d4d5a88ab1eb846db66241ce435098ce4f6c1a1
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-06-30 19:55:38 +0530

    Day 2

---
+++ TNSIF_CoreJava/src/com/tnsif/day2/IfDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.day2;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		
+		int x = 3;
+		
+		if (x == 4) {
+			System.out.println("Value of x is 4");
+		}
+	}
+}
