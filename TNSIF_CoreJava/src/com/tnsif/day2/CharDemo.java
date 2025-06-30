55453089b8a89dbf886d03af00096a779bcf16af
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-06-30 19:38:12 +0530

    Day 2

---
+++ TNSIF_CoreJava/src/com/tnsif/day2/CharDemo.java
@@ -0,0 +1,27 @@
+package com.tnsif.day2;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+			char ch ='a';
+			System.out.println(ch);
+		
+//		assigning num to cahr
+			char ch1 = 65;
+			System.out.println(ch1);
+		
+			int name='a';
+			System.out.println(name);
+		
+			char ch2 =1;
+			System.out.println(ch2);
+		
+			char ch3 ='1';
+			System.out.println(ch3);
+		
+//		assigning unicode to char
+				char var1='\u20AC';
+				System.out.println(var1);
+	}
+
+}
