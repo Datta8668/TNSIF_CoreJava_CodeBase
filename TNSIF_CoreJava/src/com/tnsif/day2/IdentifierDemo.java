5b01b8d5b44f94b70a6ae135ad9736ce2f4bc184
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-06-30 19:34:04 +0530

    Day 2

---
+++ TNSIF_CoreJava/src/com/tnsif/day2/IdentifierDemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.day2;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		
+//				int for = 23; error bcoz keyword cannot be declare as variable
+//				int num 1 = 2; error bcoz of space
+//				int @num = 12; Special are not allowed only $ and _ are allowed
+				
+//				valid declaration 
+				int num;
+				
+//				valid identification 
+				int $num1 = 23;
+				String StudentName = "Dattatray";
+				int FOR = 45;
+				
+				System.out.println("Values of the number variable is "+$num1);
+				System.out.println("Student name is "+StudentName);
+	}
+
+}
