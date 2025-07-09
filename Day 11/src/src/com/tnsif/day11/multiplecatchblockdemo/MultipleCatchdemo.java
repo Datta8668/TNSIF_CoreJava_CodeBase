996513c676c917b0d8a50d52a643a730879e4dd8
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-07-09 21:24:35 +0530

    Day 11

---
+++ Day 11/src/src/com/tnsif/day11/multiplecatchblockdemo/MultipleCatchdemo.java
@@ -0,0 +1,13 @@
+package src.com.tnsif.day11.multiplecatchblockdemo;
+
+public class MultipleCatchdemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am in main method ");
+		
+		Division.divide();
+
+	}
+
+}
