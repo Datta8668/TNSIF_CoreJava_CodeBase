42cbb16c3f60532b19d014d4108ad9ad517ab29d
Datta8668 <dattatraybhaganagare3571@gmail.com>
2025-07-09 21:18:24 +0530

    Day 1

--- Day1/src/com/tnsif/day1/Hello.java
+++ Day1/src/com/tnsif/day1/Hello.java
@@ -2,6 +2,7 @@ package com.tnsif.day1;
 
 public class Hello {
 
+//	main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(".......Hello Java.......");
